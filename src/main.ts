42b5840498ffe0317711415a04e2c8367b427b0f
Phantola <devmonarch0115@gmail.com>
2022-12-17 13:51:30 +0900

    Setting swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { PrismaService } from "./prisma.service";
 
@@ -10,6 +11,14 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("r3plica")
+    .setDescription("r3plica API")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("/docs", app, document);
 
   app.enableCors({
     origin: "*",
