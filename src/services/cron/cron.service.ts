d98fa24613d13489400f53f2ce195d23fab4e521
MG <coalsrbs7@gmail.com>
2022-12-18 03:06:26 +0900

    Update updateUserItemAndEntitlement func

--- src/services/cron/cron.service.ts
+++ src/services/cron/cron.service.ts
@@ -2,13 +2,16 @@ import { Injectable, Logger } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
 import { PrismaService } from "src/prisma.service";
 import { PlayFabService } from "../playfab/playfab.service";
+import { TESTNET_AFSBT_PROXY_CONTRACT_ADDRESS, TESTNET_QFSBT_PROXY_CONTRACT_ADDRESS } from "../../constants";
 
 @Injectable()
 export class CronService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly playFabService: PlayFabService
-  ) {}
+  ) {
+  }
+
   private readonly logger = new Logger(CronService.name);
 
   @Cron("*/3 * * * *")
@@ -26,47 +29,52 @@ export class CronService {
 
     for (let user of users) {
       let items = await this.playFabService.getUserItems(user.playFabId);
-      let entitlement = await this.playFabService.getUserEntitlements(
-        user.playFabId
-      );
-
       items = items.filter(
         (item) => item.rarity === "Epic" || item.rarity === "Legendary"
       );
 
-      const itemsCreateArray = items.map((item) => {
-        return {
-          playFabId: user.playFabId,
-          itemId: item.itemID,
-        };
-      });
+      for (const item of items) {
+        const itemToken = await this.prismaService.itemToken.findFirst(
+          {
+            where: {
+              contractAddress: TESTNET_AFSBT_PROXY_CONTRACT_ADDRESS,
+              itemId: item.itemID,
+              playFabId: null
+            }
+          }
+        );
+        await this.prismaService.itemToken.update({
+          where: {
+            tokenId: itemToken.tokenId
+          },
+          data: {
+            playFabId: user.playFabId
+          }
+        });
+      }
 
-      const entitlementCreateArray = entitlement.map((entitlement) => {
-        return {
-          playFabId: user.playFabId,
-          entitlementId: entitlement.questID,
-        };
-      });
-
-      await this.prismaService.userItem.deleteMany({
-        where: {
-          playFabId: user.playFabId,
-        },
-      });
-
-      await this.prismaService.userItem.createMany({
-        data: itemsCreateArray,
-      });
-
-      await this.prismaService.userEntitlement.deleteMany({
-        where: {
-          playFabId: user.playFabId,
-        },
-      });
-
-      await this.prismaService.userEntitlement.createMany({
-        data: entitlementCreateArray,
-      });
+      let entitlements = await this.playFabService.getUserEntitlements(
+        user.playFabId
+      );
+      for (const entitlement of entitlements) {
+        const entitlementToken = await this.prismaService.entitlementToken.findFirst(
+          {
+            where: {
+              contractAddress: TESTNET_QFSBT_PROXY_CONTRACT_ADDRESS,
+              entitlementId: entitlement.questID,
+              playFabId: null
+            }
+          }
+        );
+        await this.prismaService.entitlementToken.update({
+          where: {
+            tokenId: entitlementToken.tokenId
+          },
+          data: {
+            playFabId: user.playFabId
+          }
+        });
+      }
     }
   }
 }
