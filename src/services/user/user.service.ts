c0cc1af9d46c950a90abeea880a483528a54f955
Phantola <devmonarch0115@gmail.com>
2022-12-17 14:14:46 +0900

    Implement User service

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getUser(playFabId: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        playFabId,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException({
+        message: "User Not Found",
+        playFabId,
+      });
+    }
+
+    return user;
+  }
+}
