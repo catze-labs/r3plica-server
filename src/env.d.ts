309ace6dc483e98c950bd725ebd9ba8d6e10f810
Phantola <devmonarch0115@gmail.com>
2022-12-17 13:42:19 +0900

    Setup constants and type, utils

---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    ENVIRONMENT: 'local' | 'dev' | 'prod';
+    DATABASE_URL: string;
+    PLAY_FAB_X_SECRET_KEY: string;
+    PLAY_FAB_TITLE_ID: string;
+    PLAY_FAB_HOST: string;
+    WEB3_STORAGE_API_TOKEN: string;
+    BSC_TEST_PROVIDER: string;
+    BSC_PROVIDER: string;
+  }
+}
---
+++ src/utils.ts
@@ -0,0 +1,27 @@
+import { HttpException } from '@nestjs/common';
+
+export function makeRandomAlphaNumericString(length: number): string {
+  let result = '';
+  let characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+export function axiosReturnOrThrow(response: any) {
+  if (response['status'] === 'OK') {
+    return response['data'];
+  } else {
+    const data = response['data'];
+    const statusCode = data['code'];
+    const errorObject = {
+      message: data['errorMessage'],
+      data,
+    };
+
+    throw new HttpException(errorObject, statusCode);
+  }
+}
