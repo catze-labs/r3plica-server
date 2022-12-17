a337d1be78f825736b86610995fd86721c6e3783
Phantola <devmonarch0115@gmail.com>
2022-12-17 13:51:46 +0900

    Implement Nonce API and Business logic

--- src/routes/nonce/nonce.controller.ts
+++ src/routes/nonce/nonce.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { NonceService } from "src/services/nonce/nonce.service";
+import { GetNonceDto } from "./dto/get-nonce.dto";
+import { nonceApiResponse } from "./schema";
 
 @Controller("nonce")
-export class NonceController {}
+@ApiTags("Nonce")
+export class NonceController {
+  constructor(private readonly nonceService: NonceService) {}
+
+  @Post()
+  @ApiResponse(nonceApiResponse)
+  async createAndGetNonce(@Body() getNonceDto: GetNonceDto) {
+    const { walletAddress } = getNonceDto;
+    return await this.nonceService.createAndGetNonce(walletAddress);
+  }
+}
--- src/services/nonce/nonce.service.ts
+++ src/services/nonce/nonce.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { makeRandomAlphaNumericString } from "src/utils";
+import { Md5 } from "ts-md5";
 
 @Injectable()
-export class NonceService {}
+export class NonceService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createAndGetNonce(walletAddress: string) {
+    const randomStr: string = Md5.hashStr(
+      Date.now().toString() + makeRandomAlphaNumericString(6)
+    );
+    const value: string =
+      "\x19Signed Message:\n" + randomStr.length + randomStr;
+    const nonce = await this.prismaService.nonce.create({
+      data: {
+        address: walletAddress,
+        value,
+      },
+    });
+
+    return { nonce: nonce.value };
+  }
+
+  async getNotUsedLatestNonce(walletAddress: string) {
+    return this.prismaService.nonce.findFirst({
+      where: {
+        address: walletAddress,
+        used: false,
+      },
+      orderBy: {
+        id: "desc",
+      },
+    });
+  }
+
+  async updateNonceToUsed(id: number) {
+    await this.prismaService.nonce.update({
+      where: {
+        id,
+      },
+      data: {
+        used: true,
+      },
+    });
+  }
+}
