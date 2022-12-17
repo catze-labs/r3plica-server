ef721ef112ef1c821cd6a80f4ba0ea68fcd60486
Phantola <devmonarch0115@gmail.com>
2022-12-17 13:59:38 +0900

    Implement signature service

--- src/services/signature/signature.service.ts
+++ src/services/signature/signature.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { NonceService } from "../nonce/nonce.service";
+import { verifyMessage } from "@ethersproject/wallet";
 @Injectable()
-export class SignatureService {}
+export class SignatureService {
+  constructor(
+    private readonly nonceService: NonceService,
+    private readonly prismaService: PrismaService
+  ) {}
+
+  public async validateUserAddress(
+    apiCallerAddress: string | undefined,
+    compareWalletAddress: string | undefined
+  ) {
+    if (
+      !apiCallerAddress ||
+      !compareWalletAddress ||
+      apiCallerAddress !== compareWalletAddress
+    ) {
+      throw new UnauthorizedException({
+        message: "Permission denied",
+        data: {
+          apiCallerAddress,
+          compareWalletAddress,
+        },
+      });
+    }
+  }
+
+  public async getAddress(
+    walletAddress: string,
+    signature: string
+  ): Promise<string | undefined> {
+    const nonce = await this.nonceService.getNotUsedLatestNonce(walletAddress);
+
+    if (nonce !== null) {
+      await this.nonceService.updateNonceToUsed(nonce.id);
+
+      let recoveredAddress: string | undefined;
+      recoveredAddress = verifyMessage(nonce.value, signature);
+
+      return recoveredAddress;
+    }
+  }
+}
