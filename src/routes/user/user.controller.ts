6ff1925c0cc619ca01490f7436c0fc8cace34675
Phantola <devmonarch0115@gmail.com>
2022-12-18 17:37:16 +0900

    Move link-wallet route

--- src/routes/user/user.controller.ts
+++ src/routes/user/user.controller.ts
@@ -1,6 +1,28 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Patch } from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { PlayFabService } from "src/services/playfab/playfab.service";
+import { PatchUserWalletDto } from "./dto/patch-user-wallet.dto";
+import { linkWalletApiResponse } from "./schema";
 
 @Controller("user")
 @ApiTags("User")
-export class UserController {}
+export class UserController {
+  constructor(private readonly PlayFabService: PlayFabService) {}
+
+  @Patch("link-wallet")
+  @ApiResponse(linkWalletApiResponse)
+  async walletLink(@Body() patchUserWalletDto: PatchUserWalletDto) {
+    const { sessionTicket, walletAddress, signature } = patchUserWalletDto;
+
+    const userInfo =
+      await this.PlayFabService.validateAndGetUserInfoBySessionTicket(
+        sessionTicket
+      );
+
+    return await this.PlayFabService.updateUserWalletAddress(
+      userInfo.PlayFabId,
+      walletAddress,
+      signature
+    );
+  }
+}
