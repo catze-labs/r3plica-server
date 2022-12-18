6f03e667a01868eea17541e9c3d5572f5d0c4b58
Phantola <devmonarch0115@gmail.com>
2022-12-19 07:38:17 +0900

    Fix cron job

--- src/services/cron/cron.service.ts
+++ src/services/cron/cron.service.ts
@@ -232,16 +232,15 @@ export class CronService {
     const users = await this.prismaService.user.findMany({
       include: {
         profileToken: true,
-        profileMint: true,
+        profileMint: {
+          where: {
+            txStatus: false,
+          },
+        },
       },
     });
 
-    users.filter(
-      (user) =>
-        !user.profileToken &&
-        user.profileMint["txStatus"] !== null &&
-        user.profileMint["txStatus"] !== true
-    );
+    users.filter((user) => !user.profileToken && user.profileMint.length > 0);
 
     for (const user of users) {
       await this.web3Service.mintPAFSBT(user.playFabId);
