5737d134da2dcf75b2078f4b6ebb7795071e2ab6
Phantola <devmonarch0115@gmail.com>
2022-12-30 17:38:01 +0900

    Fix cron time schedule

--- src/services/cron/cron.service.ts
+++ src/services/cron/cron.service.ts
@@ -140,7 +140,7 @@ export class CronService {
     }
   }
 
-  @Cron("*/2 * * * *")
+  @Cron("*/1 * * * *")
   async updateUserItemAndAchievement() {
     this.logger.log("updateUserItemAndAchievement");
     const users = await this.prismaService.user.findMany({});
