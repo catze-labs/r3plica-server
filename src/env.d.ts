10a65216f41e5c620cf300e44eacf1aacc15f39b
phantola <phantoladev@gmail.com>
2023-01-05 16:23:24 +0900

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,13 +2,19 @@ declare namespace NodeJS {
   interface ProcessEnv {
     ENVIRONMENT: "local" | "dev" | "prod";
     PORT: number;
+    PRIVATE_KEY: string;
+
     DATABASE_URL: string;
+
     PLAY_FAB_X_SECRET_KEY: string;
     PLAY_FAB_TITLE_ID: string;
     PLAY_FAB_HOST: string;
-    WEB3_STORAGE_API_TOKEN: string;
+
     BSC_TEST_PROVIDER: string;
     BSC_PROVIDER: string;
-    PRIVATE_KEY: string;
+    BSCAN_API_KEY: string;
+
+    APOTHEM_PROVIDER: string;
+    SLACK_WEBHOOK_URL: string;
   }
 }
